1bf99ba0898702f871d2f63ce2f737daf44a5c7a
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2021-08-29 17:26:58 +0200

    Add files via upload

---
+++ gatk_dup_bqsr.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir mutation
+for item in $(ls *.bam)
+do
+	echo "GATK_${item%.*}"
+	ref = /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta
+	# Duplicates Marking, chip-seq,dap-seq,call snp need this step.
+	gatk --java-options "-Xmx10G -Djava.io.tmpdir=./" MarkDuplicates \
+		-I ${item%.*}.bam \
+		-O ${item%.*}_dup.bam \
+		-M ${item%.*}.metrics \
+		2>> dup_log.txt
+	 # Fix mate information of the pair-end.
+	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" FixMateInformation \
+		-I ${item%.*}_dup.bam \
+		-O ${item%.*}_dup_fixed.bam \
+		-SO coordinate \
+		2>>duFix_log.txt
+	# samtools index 
+	samtools index ${item%.*}_dup_fixed.bam
+	# base quality score recalibration
+	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./"  BaseRecalibrator \
+		-R $ref  \
+		-I ${item%.*}_dup_fixed.bam  \
+		--known-sites /proj/y.dong/GATK/hg38/dbsnp_146.hg38.vcf \
+		--known-sites /proj/y.dong/GATK/hg38/Mills_and_1000G_gold_standard.indels.hg38 \
+		-O ${item%.*}_recal.txt /
+		2>>${item%.*}_recal_log.txt
+    
+    gatk --java-options "-Xmx20G -Djava.io.tmpdir=./"   ApplyBQSR \
+		-R $ref  \
+		-I ${item%.*}_dup_fixed.bam  \
+		-bqsr ${item%.*}_recal.txt \
+		-O ${item%.*}_bqsr.bam \
+		2>>${item%.*}_bqsr_log.txt
+	 
+done
