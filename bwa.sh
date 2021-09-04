a7abe5f24283bb1805afc1c4cec2bd93b7236e7f
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2021-09-04 11:53:41 +0200

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -5,6 +5,6 @@ mkdir aligned
 for item in $(ls *.sra)
 do
 	echo "bwa_${item%.*}"
-	bwa mem -t 52 -M /DYY/Homo_sapiens/hg38/BWAIndex/genome.fa ./clipper_fastq/${item%.*}_1.fq ./clipper_fastq/${item%.*}_2.fq  > ./aligned/${item%.*}.sam 2>> ./aligned/mapping_repo.txt
+	bwa mem -t 52 -M -R "@RG\tID:${item%.*}\tSM:${item%.*}\tLB:WES\tPL:Illumina" /DYY/Homo_sapiens/hg38/BWAIndex/genome.fa ./clipper_fastq/${item%.*}_1.fq ./clipper_fastq/${item%.*}_2.fq  > ./aligned/${item%.*}.sam 2>> ./aligned/mapping_repo.txt
 	
 done
