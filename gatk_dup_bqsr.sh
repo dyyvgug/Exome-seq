fe59a20e72cf6656327f0c95c29e26efe2056474
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2021-09-07 10:27:58 +0200

    Update gatk_dup_bqsr.sh

--- gatk_dup_bqsr.sh
+++ gatk_dup_bqsr.sh
@@ -20,9 +20,22 @@ do
 	# samtools index 
 	samtools index ${item%.*}_dup_fixed.bam
 	# base quality score recalibration
-    	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" BaseRecalibrator -R /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta -I ${item%.*}_dup_fixed_add.bam --known-sites /proj/y.dong/GATK/hg38/dbsnp_146.hg38.vcf.gz --known-sites /proj/y.dong/GATK/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz -O ${item%.*}_recal.table 2>>bqsr_recal_log.txt
+    	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" BaseRecalibrator \
+		-R /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta \
+		-I ${item%.*}_dup_fixed_add.bam \
+		--known-sites /proj/y.dong/GATK/hg38/dbsnp_146.hg38.vcf.gz \
+		--known-sites /proj/y.dong/GATK/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
+		-O ${item%.*}_recal.table \
+		2>>bqsr_recal_log.txt
     
-    	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" ApplyBQSR -R /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta -I ${item%.*}_dup_fixed_add.bam -bqsr ${item%.*}_recal.table -O ${item%.*}_bqsr.bam 2>>apply_bqsr_log.txt
+    	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" ApplyBQSR \
+		-R /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta \
+		-I ${item%.*}_dup_fixed_add.bam \
+		-bqsr ${item%.*}_recal.table \
+		-O ${item%.*}_bqsr.bam \
+		2>>apply_bqsr_log.txt
 	# check BAM files for error 
-	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" ValidateSamFile -I ${item%.*}_bqsr.bam 2>> check_bam_log.txt
+   	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" ValidateSamFile \
+		-I ${item%.*}_bqsr.bam \
+		2>> check_bam_log.txt
 done
