14b6850e7faac6229b32ae848ee96367beefb3ad
KatnissD <dyyvgug@163.com>
2021-09-07 15:19:25 +0200

    modify

--- gatk_mut.sh
+++ gatk_mut.sh
@@ -5,9 +5,10 @@ mkdir mutation
 numbers=( 'SRR7829660' 'SRR7829678' 'SRR7829664' 'SRR7829675' 'SRR7829663' 'SRR7829670' 'SRR7829668' 'SRR7829673' 'SRR7829659' 'SRR7829677' 'SRR7829665' 'SRR7829676' 'SRR7829667' 'SRR7829672' 'SRR7829666' 'SRR7829669' 'SRR7829662' 'SRR7829671')
 
 i=0
+j=0
 until [ ! $i -lt 8 ]
 do
-j=0
+
 echo "patient $j is ${numbers[$j]} "
 gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" Mutect2 \
 	-R /proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta \
