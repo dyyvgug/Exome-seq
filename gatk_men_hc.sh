e66a39bc7965a5d29075e9f084766ea8d6ef97df
KatnissD <dyyvgug@163.com>
2021-09-25 12:17:10 +0200

    add

--- gatk_men_hc.sh
+++ gatk_men_hc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir mutation
+ref=/proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta
 for item in $(ls *.sam)
 do
-	ref=/proj/y.dong/GATK/hg38/Homo_sapiens_assembly38.fasta
 	# use HaplotypeCaller to identify variance
 	gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" HaplotypeCaller \
 		-ERC GVCF \
@@ -15,23 +15,24 @@ do
 	
 done
 
+cd mutation
 j=0
-for i in $(ls ./mutation/*.gz)
+for i in $(ls *.gz)
 do
 	gvcf_list[j]=$i
 	j=`expr $j + 1`
 done
 
 gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" CombineGVCFs \
-	-R $ref
-	-V ./mutation/${gvcf_list[0]}
-	-V ./mutation/${gvcf_list[1]}
-	-V ./mutation/${gvcf_list[2]}
-	-O ./mutation/cohort.g.vcf.gz
-	2>>./mutation/combine_log.txt
+	-R $ref \
+	-V ./${gvcf_list[0]} \
+	-V ./${gvcf_list[1]} \
+	-V ./${gvcf_list[2]} \
+	-O ./cohort.g.vcf.gz \
+	2>>./combine_log.txt
 	
 gatk --java-options "-Xmx20G -Djava.io.tmpdir=./" GenotypeGVCFs \
-	-R $ref
-	-V ./mutation/cohort.g.vcf.gz
-	-O ./mutation/cohort.vcf.gz
-	2>>./mutation/genotype_log.txt
+	-R $ref \
+	-V ./cohort.g.vcf.gz \
+	-O ./cohort.vcf.gz \
+	2>>./genotype_log.txt
