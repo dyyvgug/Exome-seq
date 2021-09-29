a5c1fb3239e531c7bfe1457135cea3f5b856b578
KatnissD <dyyvgug@163.com>
2021-09-29 22:21:34 +0200

    add

---
+++ annovar_database.sh
@@ -0,0 +1,5 @@
+$perl annotate_variation.pl -downdb -webfrom annovar gnomad_genome --buildver hg38 humandb/ # For filter-based annotation
+$perl annotate_variation.pl -downdb -webfrom annovar exac03 --buildver hg38 humandb/ #For filter-based annotation
+$perl annotate_variation.pl -downdb -webfrom annovar 1000g2015aug --buildver hg38 humandb/ #Filter
+$perl annotate_variation.pl -downdb -webfrom annovar dbnsfp42c --buildver hg38 humandb/ #For filter-based annotation
+$perl annotate_variation.pl -downdb -webfrom annovar intervar_20180118 --buildver hg38 humandb/ #For filter-based annotation
