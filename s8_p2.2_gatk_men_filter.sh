46fa400c4892ce686e76e84d77c2a918e1c50a45
KatnissD <dyyvgug@163.com>
2021-09-30 23:20:57 +0200

    add

--- s8_p2.2_gatk_men_filter.sh
+++ s8_p2.2_gatk_men_filter.sh
@@ -16,52 +16,53 @@ mills=$gatk_bundle/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
 if [ ! -d ./filter ]
 then mkdir ./filter
 fi
- 
+
+cd filter
 # SNP mode
 time $gatk VariantRecalibrator \
     -R $ref \
-    -V cohort.vcf.gz \
+    -V ../cohort.vcf.gz \
     --resource:hapmap,known=false,training=true,truth=true,prior=15.0 $hapmap \
     --resource:omini,known=false,training=true,truth=false,prior=12.0 $omini \
-    --resource:1000G,known=false,training=true,truth=true,prior=10.0 $G1000 \
-    --resource:dbsnp,known=true,training=false,truth=false,prior=6.0 $dbsnp \
+    --resource:1000G,known=false,training=true,truth=false,prior=10.0 $G1000 \
+    --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 $dbsnp \
     -an DP -an QD -an FS -an SOR -an ReadPosRankSum -an MQRankSum \
     -mode SNP \
     -tranche 100.0 -tranche 99.0 -tranche 99.0 -tranche 95.0 -tranche 90.0 \
-    --rscript-file ./filter/trio_snps.plots.R \
-    --tranches-file ./filter/trio_snps.tranches \
-    -O ./filter/trio_snps.recal \
-    2>>./filter/snp_log.txt
+    --rscript-file trio_snps.plots.R \
+    --tranches-file trio_snps.tranches \
+    -O trio_snps.recal \
+    2>>snp.log
  
 time $gatk ApplyVQSR \
     -R $ref \
-    -V cohort.vcf.gz \
+    -V ../cohort.vcf.gz \
     --truth-sensitivity-filter-level 99.0 \
-    --tranches-file ./filter/trio_snps.tranches \
-    --recal-file ./filter/trio_snps.recal \
+    --tranches-file trio_snps.tranches \
+    --recal-file trio_snps.recal \
     -mode SNP \
-    -O ./filter/trio_snps_VQSR.vcf.gz \
-    2>>./filter/snp_apply_log.txt
+    -O trio_snps_VQSR.vcf.gz \
+    2>>snp_apply.log
  
 # Indel mode
 time $gatk VariantRecalibrator \
     -R $ref \
-    -V ./filter/trio_snps_VQSR.vcf.gz \
+    -V trio_snps_VQSR.vcf.gz \
     --max-gaussians 4 \
     -resource:mills,known=false,training=true,truth=true,prior=15.0 $mills \
     -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
     -mode INDEL \
-    --rscript-file ./filter/trio_indels.plots.R \
-    --tranches-file ./filter/trio_indels.tranches \
-    -O ./filter/trio_indels.recal \
-    2>>./filter/indel_log.txt
+    --rscript-file trio_indels.plots.R \
+    --tranches-file trio_indels.tranches \
+    -O trio_indels.recal \
+    2>>indel.log
  
 time $gatk ApplyVQSR \
     -R $ref \
-    -V ./filter/trio_snps_VQSR.vcf.gz \
+    -V trio_snps_VQSR.vcf.gz \
     --truth-sensitivity-filter-level 99.0 \
-    --tranches-file ./filter/trio_indels.tranches \
-    --recal-file ./filter/trio_indels.recal \
+    --tranches-file trio_indels.tranches \
+    --recal-file trio_indels.recal \
     -mode INDEL \
-    -O ./filter/trio_snp_indel_VQSR.vcf.gz \
-    2>>./filter/indel_apply_log.txt
+    -O trio_snp_indel_VQSR.vcf.gz \
+    2>>indel_apply.log
---
+++ step9.1_p2_annovar_convert_anno.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! -d ./anno ]
+then mkdir ./anno
+fi
+
+for item in $(ls *.vcf)
+do
+        echo "annovar convert ${item%.*}"
+        convert2annovar.pl -format vcf4 ./${item%.*}.vcf > ./${item%.*}.avinput
+		
+		#frequent variants
+		echo "annovar annotate ${item%.*}"
+        table_annovar.pl \
+        -thread 52 \
+        ./${item%.*}.avinput \
+        /proj/y.dong/annovar/humandb \
+        -buildver hg38 \
+        -out ./anno/${item%.*} \
+        -remove \
+        -protocol refGene,clinvar_20200316,gnomad_exome \
+        -operation g,f,f \
+        -nastring . \
+        -vcfinput
+done
+
